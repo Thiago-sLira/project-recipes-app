d09acd18a50973b1b178e3d58f3f88798b6f52bc
Juan Ribeiro <juan.ribeiro3@outlook.com>
2023-01-25 20:46:30 -0300

    Requisito 8 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,46 @@
+import Button from 'react-bootstrap/Button';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
-function Header() {
+function Header({ mainPage, title }) {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/profile');
+  };
+
   return (
     <div>
-      <button
+      <h1 data-testid="page-title">
+        {title }
+      </h1>
+
+      <Button
         type="button"
         data-testid="profile-top-btn"
         className="profileButton"
+        src={ profileIcon }
+        onClick={ handleClick }
       >
-        Search
-      </button>
+        <img src={ profileIcon } alt="profileIcon" />
+      </Button>
 
+      { mainPage && (
+        <Button
+          type="button"
+          data-testid="search-top-btn"
+          className="profileButton"
+          src={ searchIcon }
+        >
+          <img src={ searchIcon } alt="profileIcon" />
+        </Button>
+      )}
     </div>
   );
 }
+
+Header.propTypes = {}.isRequired;
 
 export default Header;
--- src/pages/FavoriteRecipes/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes/FavoriteRecipes.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 function FavoriteRecipes() {
   return (
-    <div>FavoriteRecipes</div>
+    <div>
+      <Header mainPage={ false } title="Favorite Recipes" />
+    </div>
   );
 }
 
