4e6693d6a247e3adb791ad346eb1987017106647
Thiago Lira <thiago.slira@outlook.com>
2023-01-25 18:28:58 -0300

    Formulário de login com validação dos inputs de email e senha feitos

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,69 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 
+const SIX = 6;
 function Login() {
+  const [loginInputs, setLoginInputs] = useState({
+    email: '', password: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLoginInputs({
+      ...loginInputs,
+      [name]: value,
+    });
+  };
+
+  const handleInputsValidation = () => {
+    const emailRegex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+    const verifyEmailInput = emailRegex.test(loginInputs.email);
+
+    const verifyPasswordInput = loginInputs.password.length >= SIX;
+
+    if (verifyEmailInput && verifyPasswordInput) {
+      setLoginInputs({ ...loginInputs, isDisabled: false });
+    } else {
+      setLoginInputs({ ...loginInputs, isDisabled: true });
+    }
+  };
+
+  useEffect(() => {
+    handleInputsValidation();
+  }, [loginInputs.email, loginInputs.password]);
+
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <h1>LOGIN</h1>
+        <input
+          type="email"
+          name="email"
+          value={ loginInputs.email }
+          id="input-email"
+          data-testid="email-input"
+          placeholder="Email"
+          onChange={ handleChange }
+        />
+        <input
+          type="password"
+          name="password"
+          value={ loginInputs.password }
+          id="input-password"
+          data-testid="password-input"
+          placeholder="Password"
+          onChange={ handleChange }
+        />
+        <button
+          data-testid="login-submit-btn"
+          type="button"
+          disabled={ loginInputs.isDisabled }
+        >
+          Login
+        </button>
+
+      </form>
+
+    </div>
   );
 }
 
