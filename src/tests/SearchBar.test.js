65824b789d799981157d1ca72ad2be71b30eb0a2
Thiago Lira <thiago.slira@outlook.com>
2023-01-28 20:40:15 -0300

    Corrigidos erros de lint no testes do SearchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -4,6 +4,10 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
+const searchTopBtnTestId = 'search-top-btn';
+const searchInputTestId = 'search-input';
+const nameSearchRadioTestId = 'name-search-radio';
+const execSearchBtnTestId = 'exec-search-btn';
 describe('Testes para o componente SearchBar', () => {
   beforeEach(() => {
     global.fetch = jest.fn().mockResolvedValue({
@@ -13,15 +17,15 @@ describe('Testes para o componente SearchBar', () => {
   test('Se é possível digitar no input e escolher um dos radios', () => {
     renderWithRouter(<App />, { initialEntries: ['/meals'] });
 
-    const searchIcon = screen.getByTestId('search-top-btn');
+    const searchIcon = screen.getByTestId(searchTopBtnTestId);
     userEvent.click(searchIcon);
 
-    const searchInput = screen.getByTestId('search-input');
+    const searchInput = screen.getByTestId(searchInputTestId);
     userEvent.type(searchInput, 'Banana');
     expect(searchInput).toHaveValue('Banana');
 
     const ingredientRadio = screen.getByTestId('ingredient-search-radio');
-    const nameRadio = screen.getByTestId('name-search-radio');
+    const nameRadio = screen.getByTestId(nameSearchRadioTestId);
     const firstLetterRadio = screen.getByTestId('first-letter-search-radio');
 
     userEvent.click(ingredientRadio);
@@ -42,18 +46,18 @@ describe('Testes para o componente SearchBar', () => {
   test('Se o botão desabilita caso não tenha nada digitado no input ou nenhum radio tenha sido clicado', () => {
     renderWithRouter(<App />, { initialEntries: ['/meals'] });
 
-    const searchIcon = screen.getByTestId('search-top-btn');
+    const searchIcon = screen.getByTestId(searchTopBtnTestId);
     userEvent.click(searchIcon);
 
-    const searchButton = screen.getByTestId('exec-search-btn');
+    const searchButton = screen.getByTestId(execSearchBtnTestId);
     expect(searchButton).toBeDisabled();
 
-    const searchInput = screen.getByTestId('search-input');
+    const searchInput = screen.getByTestId(searchInputTestId);
     userEvent.type(searchInput, 'Banana');
     expect(searchInput).toHaveValue('Banana');
     expect(searchButton).toBeDisabled();
 
-    const nameRadio = screen.getByTestId('name-search-radio');
+    const nameRadio = screen.getByTestId(nameSearchRadioTestId);
     userEvent.click(nameRadio);
     expect(searchButton).toBeEnabled();
 
@@ -63,16 +67,16 @@ describe('Testes para o componente SearchBar', () => {
   test('Se ao clicar no botão de "Search", um fetch é chamado de acordo com a rota "/meals"', () => {
     renderWithRouter(<App />, { initialEntries: ['/meals'] });
 
-    const searchIcon = screen.getByTestId('search-top-btn');
+    const searchIcon = screen.getByTestId(searchTopBtnTestId);
     userEvent.click(searchIcon);
 
-    const searchInput = screen.getByTestId('search-input');
+    const searchInput = screen.getByTestId(searchInputTestId);
     userEvent.type(searchInput, 'Banana');
 
-    const nameRadio = screen.getByTestId('name-search-radio');
+    const nameRadio = screen.getByTestId(nameSearchRadioTestId);
     userEvent.click(nameRadio);
 
-    const searchButton = screen.getByTestId('exec-search-btn');
+    const searchButton = screen.getByTestId(execSearchBtnTestId);
     userEvent.click(searchButton);
 
     expect(fetch).toHaveBeenCalledTimes(1);
@@ -80,16 +84,16 @@ describe('Testes para o componente SearchBar', () => {
   test('Se ao clicar no botão de "Search", um fetch é chamado de acordo com a rota "/drinks"', () => {
     renderWithRouter(<App />, { initialEntries: ['/drinks'] });
 
-    const searchIcon = screen.getByTestId('search-top-btn');
+    const searchIcon = screen.getByTestId(searchTopBtnTestId);
     userEvent.click(searchIcon);
 
-    const searchInput = screen.getByTestId('search-input');
+    const searchInput = screen.getByTestId(searchInputTestId);
     userEvent.type(searchInput, 'Banana');
 
-    const nameRadio = screen.getByTestId('name-search-radio');
+    const nameRadio = screen.getByTestId(nameSearchRadioTestId);
     userEvent.click(nameRadio);
 
-    const searchButton = screen.getByTestId('exec-search-btn');
+    const searchButton = screen.getByTestId(execSearchBtnTestId);
     userEvent.click(searchButton);
 
     expect(fetch).toHaveBeenCalledTimes(1);
